c09bb18579880a1c326e1e587f776ac6e285acbe
DkXenos <superjason101010@gmail.com>
2025-06-30 16:52:23 +0700

    Create About.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-[#B2D5F1]">
+      <h1 className="text-4xl font-bold">About O-Week</h1>
+    </div>
+  );
+}

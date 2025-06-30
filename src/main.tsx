d2f762d8110ebc42f1bc61a6bdae9cd913d4412b
DkXenos <superjason101010@gmail.com>
2025-06-30 16:59:16 +0700

    router link stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import About from "./pages/About.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Navbar />
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);

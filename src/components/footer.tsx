b8fec23c071317036e7d459d97c46c1121570382
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-07-05 16:30:21 +0700

    responsive footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,8 @@
 export default function Footer(){
     return(
         <>
-        <div className="sm:h-[7rem] relative z-[10]">
-            <div className="absolute z-10 w-screen h-[1rem] sm:h-[3.5rem] bg-contain sm:bg-cover bg-[url('/elements/footer/footer-top.svg')]"></div>
-            <div className="absolute top-[0.8rem] h-[1.5rem] bg-contain sm:top-10 z-5 w-screen sm:h-[5rem] sm:bg-cover bg-[url('/elements/footer/footer-waves.svg')]"></div>
+        <div className="relative z-[10]">
+            <img src="/elements/footer/footer-group.svg" className="object-contain w-screen" alt="" />
         </div>
         <footer className="relative z-10 px-6 sm:px-12 w-screen flex items-center h-[20vh] bg-gradient-to-b from-[#3A5FB5] to-[#93C0E7]">
             <div className=" flex items-center justify-between w-screen">

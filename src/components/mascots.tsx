d1fe715692d15ec505e53b3f2b93ac3c9f3a94fa
DkXenos <superjason101010@gmail.com>
2025-07-08 15:58:49 +0700

    stable update save

--- src/components/mascots.tsx
+++ src/components/mascots.tsx
@@ -7,37 +7,54 @@ export default function Mascots() {
           className="w-full h-full relative -top-[0vh] z-[5]"
           alt="pillar-center"
         />
-        
       </div>
+
       <img
-            src="/elements/about/sunbot.svg"
-            className="absolute -top-[0vh] w-full h-auto left-1/2 -translate-x-1/2"
-            alt="sun"
-          />
-      <img
+        src="/elements/about/sunbot.svg"
+        className="absolute -top-[0vh] w-full h-auto left-1/2 -translate-x-1/2"
+        alt="sun"
+      />
+
+      <div className="absolute top-[18vh] left-1/2 -translate-x-1/2 w-[65%] z-[6]">
+        <img
           src="/elements/about/im-yucca.svg"
-          className="w-[65%] h-auto absolute top-[18vh] left-1/2 -translate-x-1/2 z-[6]"
+          className="w-full h-auto"
           alt="im-yucca"
         />
-        <p className="absolute text-center text-[2vh] text-[#AB6528] font-roboto w-[45%] mt-3 px-4 z-[10]">
-          Yucca adalah seekor unicorn lucu yang berpendirian teguh. Ia tidak mudah menyerah, 
-          bahkan mudah beradaptasi ketika menghadapi situasi yang sulit.
+
+        <p className="absolute top-[65%] left-1/2 -translate-x-1/2 text-center text-[2vh] text-[#AB6528] font-roboto w-[70%] px-4 z-[10]">
+          Yucca adalah seekor unicorn lucu yang berpendirian teguh. Ia tidak
+          mudah menyerah, bahkan mudah beradaptasi ketika menghadapi situasi
+          yang sulit.
         </p>
-      <img
+      </div>
+
+      <div className="absolute -bottom-[0vh] left-1/2 -translate-x-1/2 w-[65%] h-auto z-[6]">
+        <img
           src="/elements/about/capcipcup.svg"
-          className="w-[65%] h-auto absolute -bottom-[0vh] left-1/2 -translate-x-1/2 z-[6]"
+          className="w-full h-auto"
           alt="capcipcup"
         />
-         <img
-          src="/elements/about/pot-both.svg"
-          className="w-full h-auto absolute -bottom-[2vh] left-1/2 -translate-x-1/2 z-[6]"
-          alt="pot-both"
-        />
-        <img
-          src="/elements/about/triangle-circle.svg"
-          className="w-full h-auto absolute top-[10vh] left-1/2 -translate-x-1/2 z-[1]"
-          alt="triangle-circle"
-        />
+
+        <p className="absolute top-[50%] left-1/2 -translate-x-1/2 text-center text-[2vh] text-[#AB6528] font-roboto w-[70%] px-4 z-[10]">
+          Chap, Chip, dan Chup, adalah tiga anak ayam bersaudara yang tumbuh
+          bersama Yucca, Karakter Chap, Chip, dan Chup masing-masing
+          melambangkan nilai-nilai Universitas Ciputra, yaitu Integrity,
+          Professionalism, dan Entrepreneurship. Ciri khas mereka adalah jumlah
+          jambul mereka yang sesuai dengan hand sign I, P, dan E.
+        </p>
+      </div>
+
+      <img
+        src="/elements/about/pot-both.svg"
+        className="w-full h-auto absolute -bottom-[2vh] left-1/2 -translate-x-1/2 z-[6]"
+        alt="pot-both"
+      />
+      <img
+        src="/elements/about/triangle-circle.svg"
+        className="w-full h-auto absolute top-[10vh] left-1/2 -translate-x-1/2 z-[1]"
+        alt="triangle-circle"
+      />
     </div>
   );
 }

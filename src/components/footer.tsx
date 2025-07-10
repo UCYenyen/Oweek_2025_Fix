740b687b59abc51990b63a691bdae143321634d4
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-07-10 19:38:15 +0700

    update blurry fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,8 +8,8 @@ export default function Footer(){
         <footer className="footer-container relative z-10 px-2 h-[10vh] sm:px-12 w-screen flex items-center sm:h-[20vh] bg-gradient-to-b from-[#3A5FB5] to-[#93C0E7]">
             <div className="flex items-center justify-between w-screen">
                 <div className="flex gap-2 sm:gap-4">
-                    <img className="uc-logo w-[2.5rem] sm:w-fit" src="/elements/icons/uc-logo.svg" alt="" />
-                    <img className="oweek-logo w-[2rem] sm:w-fit" src="/elements/icons/oweek-logo.svg" alt="" />
+                    <img className="uc-logo w-[2.5rem] sm:w-fit" src="/elements/icons/uc-logo.png" alt="" />
+                    <img className="oweek-logo w-[2rem] sm:w-fit" src="/elements/icons/Oweek-Logo.png" alt="" />
                 </div>
                 <img className="luminate-footer-logo flex w-[5rem] sm:w-fit" src="/elements/icons/luminate-logo-text.png" alt="" />
                 <div className="overflow-hidden">

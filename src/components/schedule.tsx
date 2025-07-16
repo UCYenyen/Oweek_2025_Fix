43bc0d01819a79046cf1631fee77d4557ee6b20a
DkXenos <superjason101010@gmail.com>
2025-07-16 23:27:02 +0700

    save data

--- src/components/schedule.tsx
+++ src/components/schedule.tsx
@@ -1,21 +1,61 @@
-export default function Schedule() {
-  return(
-    <div className="relative z-[1000] pb-10 flex flex-col items-center justify-center gap-4">
-      {/* container untuk schedule card dan button */}
-      <div className="flex items-center justify-center gap-4">
-        <button className="bg-black rounded-full w-[50px] h-[50px]"></button>
-        <div className="schedule-card rounded-xl">
+import { useState } from "react";
+import { scheduleData } from "../data/scheduleData";
 
+export default function Schedule() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % scheduleData.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + scheduleData.length) % scheduleData.length
+    );
+  };
+
+  const currentSchedule = scheduleData[currentIndex];
+
+  return (
+    <div className="schedule-wrapper relative z-[1000] flex flex-col items-center justify-center w-full gap-4">
+      {/* container untuk schedule card dan button */}
+      <div className="flex items-center justify-center w-full gap-4">
+        <button onClick={handlePrev} className="">
+          <img src="/elements/schedule/buttonleft.svg" alt="Previous" className="w-12 h-12 button-left" />
+        </button>
+
+        <div className="flex flex-col items-center text-center schedule-card rounded-3xl">
+          <h2 className="text-2xl font-bold text-[#D45A0A] font-serif schedule-item-title">
+            {currentSchedule.title}
+          </h2>
+          <p className="mt-2 text-lg schedule-item-desc">
+            {currentSchedule.date}
+          </p>
+          <p className="text-lg schedule-item-desc">
+            {currentSchedule.location}
+          </p>
+          <div className="mt-4 text-center schedule-sessions-list">
+            {currentSchedule.sessions.map((session, index) => (
+              <p key={index} className="text-md">
+                {session}
+              </p>
+            ))}
+          </div>
         </div>
-        <button className="bg-black rounded-full w-[50px] h-[50px]"></button>
+
+        <button onClick={handleNext} className="">
+          <img src="/elements/schedule/buttonright.svg" alt="Next" className="w-12 h-12 button-right" />
+        </button>
       </div>
 
       {/* Button dresscode bawah penugasan dan ketentuan*/}
-      <div className="flex items-center justify-between w-[78%] gap-4">
-        <button>PENUGASAN</button>
-        <button className="bg-black rounded-full w-[50px] h-[50px]"></button>
-        <button>KETENTUAN</button>
+      <div className="flex items-center justify-between w-[78%] gap-4 schedule-button-group">
+        <button className="button-schedule rounded-full font-lettertype text-white">PENUGASAN</button>
+        <button className="button-dresscode">
+          <img src="/elements/schedule/shirt.svg" alt="Dress Code" />
+        </button>
+        <button className="button-schedule rounded-full font-lettertype text-white">KETENTUAN</button>
       </div>
     </div>
-  )
+  );
 }
--- src/pages/Schedule/Page.tsx
+++ src/pages/Schedule/Page.tsx
@@ -1,18 +1,23 @@
-import "./styles.css";
+// import "./styles.css";
+import "./style.css";
 import ScheduleDiv from "../../components/schedule";
 
 export default function Schedule() {
   return (
     <div className="relative flex flex-col items-center justify-center w-screen bg-[#B2D5F1] bg-cover bg-[url('/elements/real-background.svg')] schedule-page-container">
       {/* absolute images here */}
-      <img className="absolute left-0 top-0 h-full w-auto" src="/elements/schedule/pillar-left.svg" alt="" />
-      <img className="absolute right-0 top-0 h-full w-auto" src="/elements/schedule/pillar-right.svg" alt="" />
-      <img src="/elements/schedule/schedulebg.png" className="w-[50%] absolute -bottom-[8rem] z-0" alt="" />
+      <img className="absolute left-0 top-0 h-full w-auto z-[5] " src="/elements/schedule/pillar-left.svg" alt="" />
+      <img className="absolute right-0 top-0 h-full w-auto z-[5]" src="/elements/schedule/pillar-right.svg" alt="" />
+      <img src="/elements/schedule/pcbg.png " className="schedule-title-img w-[50%] absolute -bottom-[0rem] z-[6]" alt="" />
+      <img className="absolute top-0 h-auto w-full z-[4]" src="/elements/schedule/newpillar.png" alt="" />
+      <img className="absolute -bottom-2 h-auto w-[30rem] z-[5] right-0" src="/elements/schedule/ccc3.png" alt="" />
+      <img className="absolute -bottom-2 h-auto w-[30rem] z-[5] left-0" src="/elements/schedule/bebekiri.png" alt="" />
+      <img className="absolute -top-2 h-auto w-[70rem] z-[5] " src="/elements/schedule/star-circle.svg" alt="" />
 
       {/* non absolute  */}
-      <div className="flex flex-col justify-center items-center">
-        <img className="mt-[-8%] z-10" src="/elements/schedule/title.svg" alt="" />
-        <h1 className="relative z-10 mt-[20%] font-lettertype text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#3F61AD] to-[#75ABDC]"> PRA OWEEK </h1>
+      <div className="title-wrapper flex flex-col justify-center items-center">
+        <img className="title-img mt-[-8%] z-10" src="/elements/schedule/title.svg" alt="" />
+        <h1 className="title-owik relative z-10 mt-[20%] mb-[2rem] font-lettertype text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#3F61AD] to-[#75ABDC]"> PRA OWEEK </h1>
         <ScheduleDiv/>
       </div>
     </div>

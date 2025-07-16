ae1a4b6cd857c752939b6db60a93a2697c479b5d
DkXenos <superjason101010@gmail.com>
2025-07-17 02:15:49 +0700

    save again

--- src/pages/Schedule/Page.tsx
+++ src/pages/Schedule/Page.tsx
@@ -6,9 +6,10 @@ export default function Schedule() {
   return (
     <div className="relative flex flex-col items-center justify-center w-screen bg-[#B2D5F1] bg-cover bg-[url('/elements/real-background.svg')] schedule-page-container">
       {/* absolute images here */}
-      <img className="absolute left-0 top-0 h-full w-auto z-[5] " src="/elements/schedule/pillar-left.svg" alt="" />
-      <img className="absolute right-0 top-0 h-full w-auto z-[5]" src="/elements/schedule/pillar-right.svg" alt="" />
+      <img className="pillar-left absolute left-0 top-0 h-full w-auto z-[5] " src="/elements/schedule/pillar-left.svg" alt="" />
+      <img className="pillar-right absolute right-0 top-0 h-full w-auto z-[5]" src="/elements/schedule/pillar-right.svg" alt="" />
       <img src="/elements/schedule/pcbg.png " className="schedule-title-img w-[50%] absolute -bottom-[0rem] z-[6]" alt="" />
+      <img src="/elements/schedule/mbbg.png " className="schedule-title-img-mb w-[50%] absolute -bottom-[0rem] z-[6]" alt="" />
       <img className="absolute top-0 h-auto w-full z-[4]" src="/elements/schedule/mpillar.png" alt="" />
       <img className="bebekanan absolute -bottom-2 h-auto w-[30rem] z-[5] right-0" src="/elements/schedule/ccc3.png" alt="" />
       <img className="bebekiri absolute -bottom-2 h-auto w-[30rem] z-[5] left-0" src="/elements/schedule/bebekiri.png" alt="" />

92b4eefa35878f2e0fa1793f3dfc602c9923e11e
DkXenos <superjason101010@gmail.com>
2025-07-17 07:59:38 +0700

    progress update
    maap ketiduran

--- src/pages/Schedule/Page.tsx
+++ src/pages/Schedule/Page.tsx
@@ -9,11 +9,11 @@ export default function Schedule() {
       <img className="pillar-left absolute left-0 top-0 h-full w-auto z-[5] " src="/elements/schedule/pillar-left.svg" alt="" />
       <img className="pillar-right absolute right-0 top-0 h-full w-auto z-[5]" src="/elements/schedule/pillar-right.svg" alt="" />
       <img src="/elements/schedule/pcbg.png " className="schedule-title-img w-[50%] absolute -bottom-[0rem] z-[6]" alt="" />
-      <img src="/elements/schedule/mbbg.png " className="schedule-title-img-mb w-[50%] absolute -bottom-[0rem] z-[6]" alt="" />
-      <img className="absolute top-0 h-auto w-full z-[4]" src="/elements/schedule/mpillar.png" alt="" />
+      <img src="/elements/schedule/mbbg.png " className="schedule-title-img-mb w-[50%] absolute -bottom-[0rem] z-[4]" alt="" />
+      <img className="absolute top-0 h-auto w-full z-[2]" src="/elements/schedule/mpillar.png" alt="" />
       <img className="bebekanan absolute -bottom-2 h-auto w-[30rem] z-[5] right-0" src="/elements/schedule/ccc3.png" alt="" />
       <img className="bebekiri absolute -bottom-2 h-auto w-[30rem] z-[5] left-0" src="/elements/schedule/bebekiri.png" alt="" />
-      <img className="star absolute -top-2 h-auto w-[70rem] z-[5] " src="/elements/schedule/star-circle.svg" alt="" />
+      <img className="star absolute -top-2 h-auto w-[70rem] z-[3] " src="/elements/schedule/star-circle.svg" alt="" />
 
       {/* non absolute  */}
       <div className="title-wrapper flex flex-col justify-center items-center">

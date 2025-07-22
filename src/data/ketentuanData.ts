dce28338eb99f904d0bae36fc13f6c81f7e7ba65
DkXenos <superjason101010@gmail.com>
2025-07-22 17:41:06 +0700

    data push

--- src/data/ketentuanData.ts
+++ src/data/ketentuanData.ts
@@ -1,29 +1,29 @@
 const ketentuanData = [
   {
-    dayId: 0, 
+    dayId: 0,
     events: [
       {
         title: "Pra-Oweek",
         rules: [
-            "Kegiatan Pra O-WEEK akan dibagi menjadi kloter 1 dan 2.",
-            "Trainee WAJIB mengikuti kegiatan Pra O-Week yang diselenggarakan secara ONLINE via Zoom.",
-            "Trainee WAJIB memperhatikan segala informasi dan materi yang disampaikan selama kegiatan Pra - Oweek untuk mengerjakan tes yang akan diberikan.",
-            "Trainee WAJIB mengerjakan tes Mental Health Assessment, Mental Health Course, Student Development Assessment, Career Interest Assessment, dan membuat Linkedin",
+          "Kegiatan Pra O-WEEK akan dibagi menjadi kloter 1 dan 2.",
+          "Trainee WAJIB mengikuti kegiatan Pra O-Week yang diselenggarakan secara ONLINE via Zoom.",
+          "Trainee WAJIB memperhatikan segala informasi dan materi yang disampaikan selama kegiatan Pra - Oweek untuk mengerjakan tes yang akan diberikan.",
+          "Trainee WAJIB mengerjakan tes Mental Health Assessment, Mental Health Course, Student Development Assessment, Career Interest Assessment, dan membuat Linkedin",
         ],
       },
       {
         title: "Ketentuan Zoom",
         rules: [
-            "Trainee WAJIB hadir 15 menit sebelum kegiatan dimulai.",
-            "Setiap trainee yang join WAJIB rename dengan FORMAT sebagai berikut: (no kelompok) - (nama kelompok) - (nama lengkap)",
-            "Trainee WAJIB menggunakan pakaian yang sopan dan rapi.",
-            "Trainee WAJIB menyalakan kamera (on camera) sepanjang kegiatan berlangsung.",
+          "Trainee WAJIB hadir 15 menit sebelum kegiatan dimulai.",
+          "Setiap trainee yang join WAJIB rename dengan FORMAT sebagai berikut: (no kelompok) - (nama kelompok) - (nama lengkap)",
+          "Trainee WAJIB menggunakan pakaian yang sopan dan rapi.",
+          "Trainee WAJIB menyalakan kamera (on camera) sepanjang kegiatan berlangsung.",
         ],
       },
     ],
   },
   {
-    dayId: 1, 
+    dayId: 1,
     events: [
       {
         title: "INDEPENDENCE DAY CEREMONY",
@@ -77,95 +77,97 @@ const ketentuanData = [
     ],
   },
   {
-    dayId: 2, 
+    dayId: 2,
     events: [
       {
         title: "MARTIKULASI JURUSAN",
-        rules: ["Jadwal matrikulasi mengikuti jadwal program studi masing masing"],
+        rules: [
+          "Jadwal matrikulasi mengikuti jadwal program studi masing masing",
+        ],
       },
-    ]
+    ],
   },
   {
-    dayId: 3, 
+    dayId: 3,
     events: [
       {
         title: "PARENTS GATHERING",
         rules: ["Dilaksanakan secara Offline di Universitas Ciputra Surabaya."],
       },
-    ]
+    ],
   },
   {
-    dayId: 4, 
+    dayId: 4,
     events: [
       {
-        title: "Talkshow Ormawa Fest",
-        rules: ["Dilaksanakan secara Offline.",
-            "Talkshow sesi 1 (09:30 - 12:10)",
-            "Talkshow sesi 2 (13:00 - 15:40)",
-            "Talkshow dengan topik Next Gen Visionaries akan dibagi menjadi 2 sesi, trainee akan bergantian mengikuti talkshow. ",
+        title: "Ketentuan ",
+        rules: [
+          "Trainee diwajibkan untuk menggunakan kemeja Putih + Celana jeans warna gelap (Tidak sobek) + Sepatu sneakers (warna bebas) serta membawa tumblr dan peralatan makan O-Week 2025.",
+          "Dresscode: Kemeja/Blouse Putih, Celana Panjang Hitam Kain (tidak sobek & tidak ketat), Sepatu",
+        ],
+      },
+      {
+        title: "Ketentuan parkir",
+        rules: [
+          "Trainee dilarang membawa kendaraan.",
+          "Hanya boleh melakukan drop off di titik yang tersedia",
+        ],
+      },
+    ],
+  },
+  {
+    dayId: 5,
+    events: [
+      {
+        title: "Ketentuan",
+        rules: ["Trainee diperbolehkan untuk membawa sepeda motor dengan menggunakan helm dan membwa SIM.",
+          "Trainee yang membawa mobil dihimbau untuk mengisi mobil dengan minimal 4 trainee dalam satu mobil.",
 
         ],
       },
-      {
-        title: "Showcase Selling Day",
-        rules: ["Offline.",
-            "(17.00 - 18.30)",
-            "Dua orang perwakilan kelompok akan mempresentasikan produk yang akan dijual pada saat Sellinh Day. Kelompok harus menyiapkan prototype produk untuk di presentasikan",
-        ],
-      },
-      
-    ]
+    ],
   },
-   {
-    dayId: 5, 
+  {
+    dayId: 6,
     events: [
       {
-        title: "Day 2",
-        rules: ["Prodi Day (7:00 - 16:00)",
+        title: "Ketentuan parkir",
+        rules: [
+          "Trainee diperbolehkan untuk membawa sepeda motor dengan menggunakan helm dan membawa SIM.",
+          "Trainee yang membawa mobil dihimbau untuk mengisi mobil dengan minimal 4 trainee dalam satu mobil.",
         ],
       },
-    ]
+    ],
   },
-   {
-    dayId: 6, 
+  {
+    dayId: 7,
     events: [
       {
-        title: "Day 3",
-        rules: ["Pra LDK (7:00 - 15:30)",
+        title: "Ketentuan parkir",
+        rules: [
+          "Trainee diperbolehkan untuk membawa sepeda motor dengan menggunakan helm dan membawa SIM.",
+          "Trainee yang membawa mobil dihimbau untuk mengisi mobil dengan minimal 4 trainee dalam satu mobil.",
         ],
       },
-    ]
+    ],
   },
-    {
-    dayId: 7, 
-    events: [
-      {
-        title: "Day 4",
-        rules: ["Coming Soon!",
-        ],
-      },
-    ]
-  },
-    {
-    dayId: 8, 
+  {
+    dayId: 8,
     events: [
       {
         title: "Day 5",
-        rules: ["Coming Soon!",
-        ],
+        rules: ["Coming Soon!"],
       },
-    ]
+    ],
   },
-   {
-    dayId: 9, 
+  {
+    dayId: 9,
     events: [
       {
         title: "Day 6",
-        rules: ["Closing Day",
-        "Coming Soon!",
-        ],
+        rules: ["Closing Day", "Coming Soon!"],
       },
-    ]
+    ],
   },
 ];
 export default ketentuanData;
--- src/data/penugasanData.ts
+++ src/data/penugasanData.ts
@@ -80,15 +80,7 @@ const penugasanData = [
     events: [
       {
         title: "Talkshow",
-        tasks: ["Refleksi Individu (Upload TIKTOK)",
-          "Deadline (Senin, 25 Agustus 2025, pukul 23.59 WIB melalui E-Learning)"
-        ],
-      },
-       {
-        title: "Ormawa Fest",
-        tasks: ["Kartu Misi: Trainee WAJIB mengumpulkan kartu misi kepada kakak mentor masing-masing setelah sesi ORMAWAFEST 2025 berakhir.",
-          "Refleksi Pribadi di buku penugasan (Senin, 25 Agustus 2025, pukul 23.59 WIB kepada kakak mentor masing-masing)",
-          "Penugasan Kelompok (Video Kreatif TikTok) (Senin, 25 Agustus 2025, pukul 23.59 WIB melalui E-Learning)",
+        tasks: ["Coming Soon.",
         ],
       },
     ]
@@ -97,8 +89,24 @@ const penugasanData = [
     dayId: 5, 
     events: [
       {
-        title: "DAY 2",
-        tasks: ["Coming Soon."],
+        title: "Kartu Misi (Individu)",
+        tasks: ["Trainee akan mendapatkan kartu misi yang dibagikan oleh kakak mentor masing-masing.", 
+          "Trainee WAJIB mendapatkan dan menempelkan minimal 5 stiker yang telah didapatkan di kartu misi sesuai dengan ketentuan yang ada.",
+          "Trainee WAJIB mengumpulkan kartu misi kepada kakak mentor masing-masing saat registrasi day ke 4.",
+        ],
+      },
+       {
+        title: "Refleksi Pribadi (Individu)",
+        tasks: ["Trainee WAJIB membuat refleksi pribadi di buku penugasan O-Week dengan minimal 100 kata.",
+          "Penugasan individu maksimal dikumpulkan pada hari Rabu, 27 Agustus 2025, pukul 23.59 WIB kepada kakak mentor masing-masing.",
+        ],
+      },
+      {
+        title: "Video Kreatif TikTok (Kelompok)",
+        tasks: ["Setiap kelompok membuat video dengan mengikuti tren TikTok sesuai dengan link di bawah ini:  <link menyusul> ",
+          "Setiap anggota WAJIB berpartisipasi di dalam video.",
+          "Pengumpulan video maksimal hari Rabu, 27 Agustus 2025, pukul 23.59 WIB melalui E-Learn",
+        ],
       },
     ]
   },

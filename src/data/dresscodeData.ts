3ec39e81d8baaa144f8b7808c0651f4ee6f42f2a
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-08-04 13:55:32 +0700

    revisi dresscode

--- src/data/dresscodeData.ts
+++ src/data/dresscodeData.ts
@@ -33,7 +33,7 @@ export const dataDresscode = [
     pakaian: [
       {
         title: "Dresscode",
-        items: ["Kemeja putih", "Celana Panjang Hitam Formal", "Sepatu Hitam"],
+        items: ["Jas Hitam, Kemeja Warna Bebas", "Celana Panjang Hitam Formal", "Sepatu/Flat Shoes/Heels (Hitam/Putih)"],
       },
     ],
     images: {

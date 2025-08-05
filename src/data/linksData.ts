e5e1e1f3e99a47cb75ead9ed63ecfc98aaae20d6
DkXenos <superjason101010@gmail.com>
2025-08-05 17:38:17 +0700

    upd data akeh

--- src/data/linksData.ts
+++ src/data/linksData.ts
@@ -7,4 +7,8 @@ export const linkData: { [key: string]: string } = {
     linkBingo: "https://drive.google.com/drive/u/2/folders/1O5q6mERDGxinryMq3ApKFG41q51D4Txc",
     tagTumblr: "https://drive.google.com/file/d/1AEe15qyZeGSKFWasEZ4YniUmWD0hiImv/view?usp=drive_link",
     hymneUC: "https://www.youtube.com/watch?v=dSL4dNt45zQ",
-};
+    asetNameTag: "https://drive.google.com/drive/folders/1mVeYKWW5wPEhaOaND2t8e09ho7c8m7vG?usp=drive_link",
+    formAkunUC: "https://forms.gle/Pz8i7M9WBSYx9pwc6",
+    buktiTwibbon: "https://forms.gle/WHhLHibrxduyUt6C7",
+    templateTwibbon: "https://drive.google.com/drive/folders/1Szh7rtDYBPkeno5ccZ-wzHaMeT37y6MV?usp=drive_link",
+  };
